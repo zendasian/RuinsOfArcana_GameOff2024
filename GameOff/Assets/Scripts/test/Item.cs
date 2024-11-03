fb10be004771cdccfd82d7ac25cfb9f2f6f8c6a0
ThunderLord <lakshaydhami46@gmail.com>
2024-11-03 19:57:22 +0530

    new files i forgot

--- GameOff/Assets/Scripts/test/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
-public class Item : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-}
