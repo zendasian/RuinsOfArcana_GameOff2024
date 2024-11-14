0377e81054a9eb3485b9705a8d10034efddc3a06
ThunderLord <lakshaydhami46@gmail.com>
2024-11-14 11:47:47 +0530

    gitingnore now ignores temp folder and completed the scanner with timer in it.
    timer is set to 10 sec by default.

--- GameOff/Assets/Scripts/test_script/Scanner_exp.cs
+++ GameOff/Assets/Scripts/test_script/Scanner_exp.cs
@@ -1,40 +1,48 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 public class Scanner_exp : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> scanner_list = new List<GameObject>();
     List<float> distance_list = new List<float>();
     public float scanner_range = 5;
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        scanner_list.AddRange(GameObject.FindGameObjectsWithTag("items"));
-    }
-
-    // Update is called once per frame
+    bool is_scannerReady = true;
+    public float scanner_cooldown = 30f;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C) && is_scannerReady)
         {
+            scanner_list.AddRange(GameObject.FindGameObjectsWithTag("items"));
             Vector2 playerpos = gameObject.transform.position;
             foreach (GameObject scanner in scanner_list)
             {
-                Sort_distance(scanner, playerpos);
+               distance_list.Add(Vector2.Distance(scanner.transform.position, playerpos));
             }
+            if (distance_list.Min() <= scanner_range)
+            {
+                int minIndex = distance_list.IndexOf(distance_list.Min());
+                Debug.Log(scanner_list[minIndex].GetComponent<Items_Object>().item.name);
+            }
+            else
+            {
+                Debug.Log("No item found");
+            }   
+            distance_list.Clear();
+            scanner_list.Clear();
+            is_scannerReady = false;
+            Invoke("scannerTimer", scanner_cooldown);
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && !is_scannerReady)
+        {
+            Debug.Log("Scanner is not ready");
         }
     }
 
-    void Sort_distance(GameObject scanner, Vector2 playerpos)
+    private void scannerTimer()
     {
-        float plyerdistance = Vector2.Distance(scanner.transform.position, playerpos);
-                if (plyerdistance <= scanner_range)
-                {
-                    Debug.Log(scanner.GetComponent<Items_Object>().item.name);
-                    
-                }
+        is_scannerReady = true;
+        Debug.Log("Scanner is ready");
     }
         private void OnDrawGizmosSelected()
     {
